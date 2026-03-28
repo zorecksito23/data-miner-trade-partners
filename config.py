c07f77323d1de08d60b3fba27abe6a49b9a6db02
zorecksito23 <giannava783@gmail.com>
2026-03-27 18:40:29 -0600

    Add APPS_SCRIPT_URL environment variable

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-SPREADSHEET_NAME = os.getenv("SPREADSHEET_NAME", "Data Miner Home Depot")
-SHEET_SKUS = os.getenv("SHEET_SKUS", "SKUS")
-SHEET_DB_PRECIOS = os.getenv("SHEET_DB_PRECIOS", "DB Precios")
-SHEET_LOGS_ERRORES = os.getenv("SHEET_LOGS_ERRORES", "LOGS Errores")
-GOOGLE_CREDS_FILE = os.getenv("GOOGLE_CREDS_FILE", "service_account.json")
+APPS_SCRIPT_URL = os.getenv(
+    "APPS_SCRIPT_URL",
+    "https://script.google.com/macros/s/AKfycbyxBWMJvT50Xcq_LoR7xDca41iNP9Zwo5ERKgrq-V5M28eiulNl5jMvw_ko8REv-pn5/exec"
+)
+
 TIMEZONE = os.getenv("TIMEZONE", "America/Mexico_City")
